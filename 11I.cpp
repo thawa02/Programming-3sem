d02c3432478f9a17b8c018852a4db94f102819d8
Костина Катя <71439209+thawa02@users.noreply.github.com>
2021-10-30 12:44:12 +0300

    Update 11I.cpp

--- 11I.cpp
+++ 11I.cpp
@@ -17,14 +17,16 @@
 
 std::vector<int> z_function(const std::string& s) {
 	std::vector<int> z(s.size(), 0);
-	int l = -1, r = -1;
+	int left = -1, right = -1;
 	for (int i = 1; i < s.size(); ++i) {
-		if (l <= i && i <= r) {
-			z[i] = std::min(z[i - l], r - i + 1);
+		if (left <= i && i <= right) {
+			z[i] = std::min(z[i - left], right - i + 1);
 		}
-		while (i + z[i] < s.size() && s[z[i]] == s[z[i] + i]) ++z[i];
-		if (i + z[i] - 1 > r) {
-			l = i; r = i + z[i] - 1;
+		while (i + z[i] < s.size() && s[z[i]] == s[z[i] + i]) {
+			++z[i];
+		}
+		if (i + z[i] - 1 > right) {
+			left = i; right = i + z[i] - 1;
 		}
 	}
 	return z;
